4da425ae40cee333e7e3e585a2d9c10f1a39e5de
Jean Carlo EM <jeancarlo@jeancarloem.com>
2023-12-17 10:44:20 -0300

    adicionado forceInclude e forceIgnore, e deleção de aruivos ignorado 2

---
+++ .maker/forceInclude.js
@@ -0,0 +1,7 @@
+/**
+ * one individual point = [latitude, longitude]
+ * range of coordinates = [start_latitude, startlongitude, end_latitude, endlongitude]
+ *
+ * forceInclude takes precedence over other options
+ */
+export const forceInclude = []
